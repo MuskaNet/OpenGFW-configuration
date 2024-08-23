d3d2d2a8c19c059b3b7112c9d6a154ad858b3eb3
夏沫花火zzz <ami@muska.zip>
2024-08-23 22:37:58 +0800

    Create update-rules.sh

---
+++ update-rules.sh
@@ -0,0 +1,5 @@
+echo "Download rules.yml"
+wget https://proxy-gh.1l1.icu/github.com/MuskaNet/OpenGFW-configuration/raw/main/rules.yml -O /opt/opengfw/rules.yml
+
+echo "Restart OpenGFW"
+systemctl restart opengfw.service
